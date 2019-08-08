88017294314cad6facc5d932926b35ff8964edbe
Andrew Zajac <ajzajac@icloud.com>
2019-08-08 20:46:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   monopoly = {
-    :railroads => {
-      :pieces => 4,{
+    :railroads =>
+    {
+      :pieces => 4,
+      {
       :rent_in_dollars => {
        :one_piece_owned=>25,
        :two_pieces_owned=>50,
